b7e0ed92092632a3106e6bd8bea9a4dbc38ad36e
garage1 <g1@omega-g2.uoregon.edu>
2012-05-22 17:10:27 -0700

    2

--- up.js
+++ up.js
@@ -1,7 +1,8 @@
 var request = require('request')
   , jsdom = require('jsdom')
   , email = require('mailer')
-  , credentials = require('./credentials');
+  , credentials = require('./credentials')
+  , fs = require('fs');
 
 function checkSite(siteObj) {
 	var url = siteObj.url
@@ -12,8 +13,11 @@ function checkSite(siteObj) {
 	
 	request({ uri: url }, function (error, response, body) {
 
+   	  var responseTime = new Date() - startTime
+
 	  if (error && response.statusCode !== 200) {
 	    callback({
+			'url': url,
 			'result': 'failure', 
 			'data': error, 
 			'time': responseTime
@@ -21,8 +25,6 @@ function checkSite(siteObj) {
 	  } 
 
 	  jsdom.env({ html: body, scripts: ['http://code.jquery.com/jquery-1.5.min.js'] }, function (err, window) {
-		
-		var responseTime = new Date() - startTime
 		
 	    var $ = window.jQuery;
 		var passing = true;
@@ -40,12 +42,14 @@ function checkSite(siteObj) {
 		// tests done, let's see if we're winning...			
 		if (passing == true) {
 			callback({
-			'result': 'success', 
+				'url': url,
+				'result': 'success', 
 				'data': [], 
 				'time': responseTime
 			})
 		} else {
 			callback({
+				'url': url,
 				'result': 'failure', 
 				'data': failedTests, 
 				'time': responseTime
@@ -56,26 +60,58 @@ function checkSite(siteObj) {
 	});	//end of request callback	
 } //end of checkSite()
 
-function notify(email, reason) {
+function notify(emailAddr, reason) {
 	console.log('notify wants to email', email, 'about', reason)
+	
+	var lastSent = 0;
+	try {
+		lastSent = fs.readFileSync(emailAddr);
+		lastSent = parseInt(lastSent.toString('ascii'));
+		console.log(lastSent)
+	} catch (e) {
+		// no file, so let's move on. probabaly the first send.
+	}
+	
+	var timeDiff = new Date() - lastSent;
+	var thresholdToSendAgain = 1000*60*5 // 1000 ms/secs * 60 secs/min * (x) min
+	
+	if (timeDiff > thresholdToSendAgain) {
+	
+		console.log("SENDING EMAIL!");
+	
+		// http://docs.sendgrid.com/documentation/get-started/integrate/examples/nodejs-example-using-smtp/
+		email.send({
+		  host : "smtp.sendgrid.net",              
+		  port : "25",                     
+		  domain : "smtp.sendgrid.net",            
+		  to : emailAddr,
+		  from : "no-reply@dailyemerald.com",
+		  subject : "SERVER DOWN",
+		  body: "Hello! This is a test of the node_mailer.",
+		  authentication : "login",      
+		  username : credentials.username,      
+		  password : credentials.password       
+		}, function(err, result){
+			if(err){ console.log(err); }
+		
+			var lastEmailSent = '' + Date.now();
 
-	email.send({
-	  host : "smtp.sendgrid.net",              // smtp server hostname
-	  port : "25",                     // smtp server port
-	  domain : "smtp.sendgrid.net",            // domain used by client to identify itself to server
-	  to : "ivong@dailyemerald.com",
-	  from : "no-reply@dailyemerald.com",
-	  subject : "node_mailer test email",
-	  body: "Hello! This is a test of the node_mailer.",
-	  authentication : "login",        // auth login is supported; anything else is no auth
-	  username : (new Buffer(credentials.username)).toString("base64"),       // Base64 encoded username
-	  password : (new Buffer(credentials.password)).toString("base64")        // Base64 encoded password
-	},
-	function(err, result){
-	  if(err){ console.log(err); }
-	});
+			fs.writeFile(emailAddr, lastEmailSent, function (err) {
+				if (err) throw err;
+				console.log('It\'s saved!');
+			});
+		});
+	} else {
+		console.log((thresholdToSendAgain-timeDiff)/1000, "seconds until we can send again.")
+	}
+	
+	
 	
 }
+
+// --------------------
+// now the fun begins
+// --------------------
 
 checkSite({
 	url: 'http://dailyemerald.com/',
@@ -84,12 +120,7 @@ checkSite({
 		{selector: 'img', hasAtLeast: 100}
 	],
 	callback: function(res) {
-		if (res.result == 'failure') {
-			notify('ivar@ivarvong.com', res.data);
-			console.log('--- dailyemerald.com fails.',res.responseTime)
-		} else {
-			console.log('+++ dailyemerald.com passes!',res.responseTime)
-		}
+		if (res.result == 'failure') notify(credentials.myemail, res.data);
 	}
 });
 
@@ -100,12 +131,7 @@ checkSite({
 		{selector: 'input', hasAtLeast: 3}
 	],
 	callback: function(res) {
-		if (res.result == 'failure') {
-			notify('ivar@ivarvong.com', res.data);
-			console.log('--- ODE WP login fails',res.responseTime)
-		} else {
-			console.log('+++ ODE WP login passes!',res.responseTime)
-		}
+		if (res.result == 'failure') notify(credentials.myemail, res.data);
 	}
 })
 
@@ -116,11 +142,6 @@ checkSite({
 		{selector: '#flag', hasAtLeast: 1}
 	],
 	callback: function(res) {
-		if (res.result == 'failure') {
-			notify('ivar@ivarvong.com', res.data);
-			console.log("--- THEGARAGE", 'fails :(',res.responseTime)
-		} else {
-			console.log("+++ THE GARAGE", 'passes!',res.responseTime)
-		}
+		if (res.result == 'failure') notify(credentials.myemail, res.data);
 	}
 })
